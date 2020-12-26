e4249db4150c218abfcc779f0d0415c61211808e
kkwietniewski <kkwietniewski@edu.cdv.pl>
2020-12-26 22:57:06 +0100

    Logo, prettier, menuDropdowns, toTopBtn, shadow content

--- script.js
+++ script.js
@@ -1,24 +1,45 @@
-let navbar = document.querySelector('.navbar');
-let searchInput = document.querySelector('.searchInput');
+// let navbar = document.querySelector(".navbar");
+// let searchInput = document.querySelector(".searchInput");
+let comeToTop = document.querySelector(".comeToTop");
 
-window.addEventListener('scroll', ()=>{
-     console.log(window.scrollY)
-    if(window.scrollY > 75) {
-        document.querySelector('.navbar').classList.add('red');
-        navbar.style.background = '';
-    }
-    else{
-        document.querySelector('.navbar').classList.remove('red');
-    }
-    
+//Top scroll button
+window.addEventListener("scroll", () => {
+  //   cconsole.log(window.scrollY);
+  if (window.scrollY > 75) {
+    comeToTop.style.display = "block";
+  } else {
+    comeToTop.style.display = "none";
+  }
 });
-// window.addEventListener('click', (ew)=>{
-//     let target = ew.target;
-//     console.log(target);
-//     searchInput.addEventListener('click', (es)=>{
-//         console.log(es.target);
-//         if(target == es.target) searchInput.parentElement.style.flexGrow = 1;
-//         else searchInput.parentElement.style.minWidth = '30%';
-//     });
+comeToTop.addEventListener("click", () => {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+});
 
-// });
+//Shadow content if mouseover navbar item
+let div = document.createElement("div");
+div.style.zIndex = 1;
+div.style.height = "100%";
+div.style.width = "100%";
+div.style.background = "#000";
+div.style.position = "absolute";
+div.style.top = 0;
+div.style.left = 0;
+div.style.opacity = 0.6;
+
+let navItem = document.querySelectorAll(".nav-item");
+
+function shadowContent(eventType) {
+  if (eventType == "mouseover" || eventType == "click")
+    document.querySelector("body").appendChild(div);
+  else if (eventType == "mouseout")
+    document.querySelector("body").removeChild(div);
+}
+navItem.forEach((item) => {
+  item.addEventListener("mouseover", (e) => {
+    shadowContent(e.type);
+  });
+  item.addEventListener("mouseout", (e) => {
+    shadowContent(e.type);
+  });
+});
