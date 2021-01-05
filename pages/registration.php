a9cee52ac67e17c54fb3e0808cebd7a5e6c1a4f0
kkwietniewski <kkwietniewski@edu.cdv.pl>
2021-01-05 19:25:50 +0100

    Rejestracja - style

--- pages/registration.php
+++ pages/registration.php
@@ -3,38 +3,63 @@
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Document</title>
-    <style rel="stylesheet" href="../style/style.css"></style>
+    <title>Registration</title>
+    <link rel="stylesheet" href="../style/register.css" />
   </head>
   <body>
-    <?php
-require_once "../scripts/register.php";
-?>
-    <form action="" method="post">
-      <div class="wrapper fadInDown">
-        <div>
-          <div>Login</div>
-          <input type="text" name="login" placeholder="login" />
-        </div>
-        <div>
-          <div>Email</div>
-          <input type="text" placeholder="email" name="email" />
-        </div>
-        <div>
-          <div>Hasło</div>
-          <input type="password" placeholder="hasło" name="pass" />
-        </div>
-        <div class="input-group my-4">
-          <div>Powtorz hasło</div>
-          <input type="password" placeholder="hasło" name="validPass" />
-        </div>
-        <div>
-          <div>
-            <a href="./logowanie.php">Zaloguj sie</a>
+    <div class="wrapper fadInDown">
+      <div id="formContent">
+        <form action="" method="post">
+          <div class="fadeIn first">
+            <img src="../images/logo200x76.png" alt="Computer Shop" />
           </div>
-          <button type="submit" name="register">Zarejestruj się</button>
+          <div class="fadeIn second">
+            <?php
+            require_once "../scripts/register.php";
+            ?>
+            <input type="text" name="login" placeholder="Podaj login" />
+          </div>
+          <div>
+            <input
+              class="fadeIn third"
+              type="text"
+              placeholder="Podaj email"
+              name="email"
+            />
+          </div>
+          <div>
+            <input
+              class="fadeIn fourth"
+              type="password"
+              placeholder="Podaj hasło"
+              name="pass"
+            />
+          </div>
+          <div>
+            <input
+              class="fadeIn fifth"
+              type="password"
+              placeholder="Powtórz hasło"
+              name="validPass"
+            />
+          </div>
+          <button type="submit" class="fadeIn sixth" name="register">
+            Zarejestruj się
+          </button>
+        </form>
+        <div id="formFooter">
+          <div class="row">
+            <div class="col-sm-12 col-md-6 col-lg-6">
+              <a class="underlineHover" href="./odzyskajHaslo.php"
+                >Odzyskaj hasło</a
+              >
+            </div>
+            <div class="col-sm-12 col-md-6 col-lg-6">
+              <a class="underlineHover" href="./logowanie.php">Zaloguj sie</a>
+            </div>
+          </div>
         </div>
       </div>
-    </form>
+    </div>
   </body>
 </html>
