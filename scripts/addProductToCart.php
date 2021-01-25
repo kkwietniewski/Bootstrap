e1e69b06ac8044faf946de8050eb7edfde49778a
Monika Nogała <mxnogala@gmail.com>
2021-01-25 14:39:19 +0100

    add to cart

--- scripts/addProductToCart.php
+++ scripts/addProductToCart.php
@@ -1,5 +1,17 @@
 <?php
   session_start();
+
+  if ($_SESSION['product_counter'] == 0)
+  {
+    $_SESSION['cart'] = array();
+  }
+
+  array_push($_SESSION['cart'], $_POST['product']);
   $_SESSION["product_counter"]++;
-  header("Location: ../pages/searchProducts.php")
+  
+  for ($i = 0; $i < count($_SESSION['cart']); $i += 1)
+  {
+    echo $_SESSION['cart'][$i]."</br>";
+  }
+  // header("Location: ../pages/searchProducts.php")
 ?>
