4afe4974237342eba575510ee8a478b00e91e03f
Kacper Kwietniewski <48916089+kkwietniewski@users.noreply.github.com>
2021-01-26 20:50:32 +0100

    ProductRating, navbar - product update

--- scripts/getNavbar.php
+++ scripts/getNavbar.php
@@ -32,38 +32,26 @@ echo <<<NAV
                 data-bs-toggle="dropdown"
                 aria-expanded="false"
               >
-              <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-cpu" viewBox="0 0 16 16">
-                <path d="M5 0a.5.5 0 0 1 .5.5V2h1V.5a.5.5 0 0 1 1 0V2h1V.5a.5.5 0 0 1 1 0V2h1V.5a.5.5 0 0 1 1 0V2A2.5 2.5 0 0 1 14 4.5h1.5a.5.5 0 0 1 0 1H14v1h1.5a.5.5 0 0 1 0 1H14v1h1.5a.5.5 0 0 1 0 1H14v1h1.5a.5.5 0 0 1 0 1H14a2.5 2.5 0 0 1-2.5 2.5v1.5a.5.5 0 0 1-1 0V14h-1v1.5a.5.5 0 0 1-1 0V14h-1v1.5a.5.5 0 0 1-1 0V14h-1v1.5a.5.5 0 0 1-1 0V14A2.5 2.5 0 0 1 2 11.5H.5a.5.5 0 0 1 0-1H2v-1H.5a.5.5 0 0 1 0-1H2v-1H.5a.5.5 0 0 1 0-1H2v-1H.5a.5.5 0 0 1 0-1H2A2.5 2.5 0 0 1 4.5 2V.5A.5.5 0 0 1 5 0zm-.5 3A1.5 1.5 0 0 0 3 4.5v7A1.5 1.5 0 0 0 4.5 13h7a1.5 1.5 0 0 0 1.5-1.5v-7A1.5 1.5 0 0 0 11.5 3h-7zM5 6.5A1.5 1.5 0 0 1 6.5 5h3A1.5 1.5 0 0 1 11 6.5v3A1.5 1.5 0 0 1 9.5 11h-3A1.5 1.5 0 0 1 5 9.5v-3zM6.5 6a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5h-3z"/>
+              <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-mouse" viewBox="0 0 16 16">
+                <path d="M8 3a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 3zm4 8a4 4 0 0 1-8 0V5a4 4 0 1 1 8 0v6zM8 0a5 5 0 0 0-5 5v6a5 5 0 0 0 10 0V5a5 5 0 0 0-5-5z"/>
               </svg>
-                Podzespoły komputerowe
+                Peryteria komputerowe
               </a>
               <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
                 <li>
                   <a class="dropdown-item" href="#"
-                    >Karty graficzne<img
-                      src="../images/product.jpg"
-                      alt=""
-                      srcset=""
-                      style="width: 60px; height: 60px"
-                  /></a>
+                    >Skanery i drukarki
+                  </a>
                 </li>
                 <li>
                   <a class="dropdown-item" href="#"
-                    >Procesory<img
-                      src="../images/product.jpg"
-                      alt=""
-                      srcset=""
-                      style="width: 60px; height: 60px"
-                  /></a>
+                    >Monitory
+                  </a>
                 </li>
                 <li>
                   <a class="dropdown-item" href="#"
-                    >Płyty główne<img
-                      src="../images/product.jpg"
-                      alt=""
-                      srcset=""
-                      style="width: 60px; height: 60px"
-                  /></a>
+                    >Tablety graficzne
+                  </a>
                 </li>
               </ul>
             </li>
@@ -84,30 +72,18 @@ echo <<<NAV
               <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
                 <li>
                   <a class="dropdown-item" href="#"
-                    >Do gier<img
-                      src="../images/product.jpg"
-                      alt=""
-                      srcset=""
-                      style="width: 60px; height: 60px"
-                  /></a>
+                    >Do gier
+                  </a>
                 </li>
                 <li>
                   <a class="dropdown-item" href="#"
-                    >Notebooki<img
-                      src="../images/product.jpg"
-                      alt=""
-                      srcset=""
-                      style="width: 60px; height: 60px"
-                  /></a>
+                    >Notebooki
+                  </a>
                 </li>
                 <li>
                   <a class="dropdown-item" href="#"
-                    >Biurowe<img
-                      src="../images/product.jpg"
-                      alt=""
-                      srcset=""
-                      style="width: 60px; height: 60px"
-                  /></a>
+                    >Biurowe
+                  </a>
                 </li>
               </ul>
             </li>
@@ -129,74 +105,18 @@ echo <<<NAV
               <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
                 <li>
                   <a class="dropdown-item" href="#"
-                    >Smartfony<img
-                      src="../images/product.jpg"
-                      alt=""
-                      srcset=""
-                      style="width: 60px; height: 60px"
-                  /></a>
+                    >Smartfony
+                  </a>
                 </li>
                 <li>
                   <a class="dropdown-item" href="#"
-                    >Smartwatche<img
-                      src="../images/product.jpg"
-                      alt=""
-                      srcset=""
-                      style="width: 60px; height: 60px"
-                  /></a>
+                    >Smartwatche
+                  </a>
                 </li>
                 <li>
                   <a class="dropdown-item" href="#"
-                    >Akcesoria do telefonów<img
-                      src="../images/product.jpg"
-                      alt=""
-                      srcset=""
-                      style="width: 60px; height: 60px"
-                  /></a>
-                </li>
-              </ul>
-            </li>
-            <li class="nav-item dropdown">
-              <a
-                class="nav-link dropdown-toggle"
-                href="#"
-                id="navbarDropdown"
-                role="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-              >
-              <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-mouse" viewBox="0 0 16 16">
-                <path d="M8 3a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 3zm4 8a4 4 0 0 1-8 0V5a4 4 0 1 1 8 0v6zM8 0a5 5 0 0 0-5 5v6a5 5 0 0 0 10 0V5a5 5 0 0 0-5-5z"/>
-              </svg>
-                Akcesoria komputerowe
-              </a>
-              <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <li>
-                  <a class="dropdown-item" href="#"
-                    >Głośniki i słuchawki<img
-                      src="../images/product.jpg"
-                      alt=""
-                      srcset=""
-                      style="width: 60px; height: 60px"
-                  /></a>
-                </li>
-                <li>
-                  <a class="dropdown-item" href="#"
-                    >Mikrofony<img
-                      src="../images/product.jpg"
-                      alt=""
-                      srcset=""
-                      style="width: 60px; height: 60px"
-                  /></a>
-                </li>
-                <li>
-                  <a class="dropdown-item" href="#"
-                    >Obudowy<img
-                      src="../images/product.jpg"
-                      alt=""
-                      srcset=""
-                      style="width: 60px; height: 60px"
-                  /></a>
+                    >Akcesoria do telefonów
+                  </a>
                 </li>
               </ul>
             </li>
