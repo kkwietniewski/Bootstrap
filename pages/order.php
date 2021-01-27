fb6320ce46c78166345e4d32f0486dcdb5e64f6f
Kacper Kwietniewski <48916089+kkwietniewski@users.noreply.github.com>
2021-01-27 18:24:08 +0100

    Order front

--- pages/order.php
+++ pages/order.php
@@ -24,15 +24,15 @@
           <div class="col-lg-2 d-flex"></div>
           <div class="cartBody col-lg-8">
             <div class="cartTitle">
-              <h2>Dostawa i płatność</h2>
+              <h2>Wybór sposobu płatności i dostawy</h2>
             </div>
             <div class="row">
               <hr>
               <div class="row">
-                <div class="col-lg-8">
+                <div class="col-lg-7">
                   <form action="#" method="post">
-                  <h4>Dostawa i płatność</h4>
                     <div class="fadeIn first">
+                      <h4>Dane odbiorcy</h4>
                       <?php
                     if (isset($_SESSION['error']))
                       {
@@ -51,15 +51,77 @@
                     <div>
                       <input class="regInp fadeIn fourth" name="city" type="text" placeholder="Podaj miasto"/>
                     </div>
+                    <div>
+                      <input class="regInp fadeIn fifth" name="email" type="text" placeholder="Podaj email"/>
+                    </div>
+                    <div>
+                      <input class="regInp fadeIn sixth" name="telephone" type="number" placeholder="Podaj telefon"/>
+                    </div>
+                    <div class="row fadeIn sixth">
+                      <h4>Płatność i dostawa</h4>
+                      <div class="col-lg-6 payment">
+                        <div class="form-check">
+                          <input class="form-check-input" type="radio" name="paymentRatios" id="paymentRatio1" value="5.00" checked>
+                          <label class="form-check-label" for="paymentRatio1">
+                            <p>Kartą<i> (5zł)</i></p>
+                          </label>
+                        </div>
+                        <div class="form-check">
+                          <input class="form-check-input" type="radio" name="paymentRatios" id="paymentRatio2" value="3.00">
+                          <label class="form-check-label" for="paymentRatio2">
+                          <p>Gotówką<i> (3zł)</i></p>
+                          </label>
+                        </div>
+                        <div class="form-check">
+                          <input class="form-check-input" type="radio" name="paymentRatios" id="paymentRatio3" value="0.00">
+                          <label class="form-check-label" for="paymentRatio3">
+                          <p>Blik<i> (0zł)</i></p>
+                          </label>
+                        </div>
+                      </div>
+                      <div class="col-lg-6 supply fadeIn sixth">
+                        <div class="form-check">
+                          <input class="form-check-input" type="radio" name="supplyRatios" id="supplyRatio1" value="8.99" checked>
+                          <label class="form-check-label" for="supplyRatio1">
+                          <p>Paczkomat<i> (8,99zł)</i></p>
+                          </label>
+                        </div>
+                        <div class="form-check">
+                          <input class="form-check-input" type="radio" name="supplyRatios" id="supplyRatio2" value="14.99">
+                          <label class="form-check-label" for="supplyRatio2">
+                          <p>Na podany adres<i> (14,99zł)</i></p>
+                          </label>
+                        </div>
+                        <div class="form-check">
+                          <input class="form-check-input" type="radio" name="supplyRatios" id="supplyRatio3" value="0.00">
+                          <label class="form-check-label" for="supplyRatio3">
+                          <p>Odbiór osobisty<i> (0zł)</i></p>
+                          </label>
+                        </div>
+                      </div>
+                    </div>
                   <div>
                 </div>
               </div>
-              <div class="col-lg-4"></div>
-            </div>
-            <div class="row">
-              <div class="col-lg-8"></div>
-              <div class="col-lg-4">
-                <button type="submit" class="btn btn-primary" style="width: 100%; color:white;">Zapłać</button>
+              <div class="col-lg-5">
+                <div class="payContainer">
+                    <h4>Wybrane produkty</h4>
+                    <?php
+                  require_once "../scripts/showCart.php";
+                    ?>
+                    <hr>
+                    <div class="row">
+                      <div class="col-10">
+                        <p>Forma płatności</p>
+                        <p>Dostawa</p>
+                      </div>
+                      <div class="col-2">
+                        <p>5</p>
+                        <p>8,99</p>
+                      </div>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Zapłać</button>
+                  </div>
               </div>
             </div>
                   </form>
