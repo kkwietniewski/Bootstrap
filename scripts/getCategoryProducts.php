f10231922a4e2ffcf50012a82136e2ed31cc0500
Monika Nogała <mxnogala@gmail.com>
2021-01-28 12:54:02 +0100

    filtering products

--- scripts/getCategoryProducts.php
+++ scripts/getCategoryProducts.php
@@ -2,29 +2,100 @@
 
     require_once '../scripts/connect.php';
 
-    if(isset($_GET['category']) || isset($_GET['subcategory']))
+    if(isset($_GET['category']) || isset($_GET['subcategory']) || isset($_GET['producer']) || isset($_GET['model']) 
+    || isset($_GET['diagonal']) || isset($_GET['priceFrom']) || isset($_GET['priceTo']))
     {
-        if (!empty($_GET['subcategory']))
+        $sql = array(); 
+
+        // if (!empty($_SESSION['category']))
+        // {
+        //     $type = 'category'; 
+        //     unset($_SESSION['category']); 
+        // }
+        // else if (!empty($_SESSION['subcategory']))
+        // {
+        //     $type = 'subcategory';
+        //     unset($_SESSION['subcategory']);
+        // }
+        // else 
+        // {
+        //     $type = "all"; 
+        // }
+
+        
+  
+        //  if(!empty($_POST['model']))
+        // {
+        //     array_push($sql, sprintf("model= '%s' ", $_POST['model']));
+        // }
+        // else if(!empty($_POST['diagonal']))
+        // {
+        //     array_push($sql, sprintf("model= '%s' ", $_POST['diagonal']));
+        // }
+        // else if(!empty($_POST['priceFrom']))
+        // {
+        //     array_push($sql, sprintf("model= '%s' ", $_POST['priceFrom']));
+        // }
+        // else if(!empty($_POST['priceTo']))
+        // {
+        //     array_push($sql, sprintf("model= '%s' ", $_POST['priceTo']));
+        // }
+
+        if (!empty($_GET['subcategory']) )
         {
             $subcategory = $_GET['subcategory']; 
+            $value = $_GET['subcategory']; 
             $name = 'subcategory_name'; 
-            $query = sprintf("SELECT p.product_id, p.product_name, p.label, p.price, p.img_src, p.availability, p.weight, p.subcategory, 
-            p.brand, p.description AS productDescription, p.store_label, p.diagonal, p.processor, p.graphic, p.color, p.resolution, 
-            s.subcategory_name, s.subcategory_description AS categoryDescription 
-            FROM products AS p JOIN subcategory AS s ON p.subcategory = s.subcategory_id 
-            WHERE s.subcategory_name = '%s' ", mysqli_real_escape_string($conn,$subcategory));        
         }
         else if (!empty($_GET['category']))
         {
             $category = $_GET['category']; 
+            $value = $_GET['category']; 
             $name = 'category_name';
-            $query = sprintf("SELECT p.product_id, p.product_name, p.label, p.price, p.img_src, p.availability, p.weight, p.subcategory, p.brand, 
-            p.description AS productDescription, p.store_label, p.diagonal, p.processor, p.graphic, p.color, p.resolution, c.category_name, c.description 
-            AS categoryDescription FROM products AS p JOIN subcategory AS s ON p.subcategory = s.subcategory_id 
-            JOIN category AS c ON s.category_id = c.category_id WHERE category_name = '%s' ", mysqli_real_escape_string($conn,$category));
+        }
+        else if (!empty($_SESSION['subcategory']))
+        {
+            $value = $_SESSION['subcategory']; 
+            $name = 'subcategory_name'; 
+            unset($_SESSION['subcategory']);
+        }
+        else if (!empty($_SESSION['category']))
+        {
+            $value = $_SESSION['category']; 
+            $name = 'category_name'; 
+            // unset($_SESSION['category']); 
         }
 
+        $query = sprintf("SELECT p.product_id, p.product_name, p.label, p.price, p.img_src, p.availability, p.weight, p.subcategory, 
+        p.brand, p.description AS productDescription, p.store_label, p.diagonal, p.processor, p.graphic, p.color, p.resolution, 
+        c.category_name, c.description AS categoryDescription, s.subcategory_name, s.subcategory_description FROM products AS p 
+        JOIN subcategory AS s ON p.subcategory = s.subcategory_id JOIN category AS c ON s.category_id = c.category_id 
+        WHERE $name='%s' ",  mysqli_real_escape_string($conn,$value));  
+        if (!empty($_GET['producer']))
+        {
+            $query = $query.sprintf(" AND p.brand = '%s' ", $_GET['producer']); 
+           
+        }
+        if(!empty($_GET['model']))
+        {
+            $query = $query.sprintf(" AND p.label = '%s' ", $_GET['model']);
+        }
+        if(!empty($_GET['diagonal']))
+        {
+            $query = $query.sprintf(" AND p.diagonal = '%s' ", $_GET['diagonal']);
+        }
+        // if(!empty($_GET['priceFrom']))
+        // {
+        //     $query = $query.sprintf(" AND p.label = '%s' ", $_GET['model']);
+        // }
+        // if(!empty($_GET['priceTo']))
+        // {
+        //     array_push($sql, sprintf("model= '%s' ", $_GET['priceTo']));
+        // }
+
         $productsArray = array();
+        $brands = array();
+        $diagonals = array();
 
         try 
         {
@@ -34,9 +105,16 @@
             }
             else 
             {
-                
                 if ($result = @$conn->query($query))
                 {
+                    if (!empty($subcategory))
+                    {
+                        $_SESSION['subcategory'] = $subcategory;
+                    }
+                    else if (!empty($category))
+                    {
+                        $_SESSION['category'] = $category; 
+                    }
 
                     while ($row = mysqli_fetch_assoc($result))
                     {
@@ -59,16 +137,18 @@
                         array_push($product, $row['subcategory']);
                         //8 - Marka
                         array_push($product, $row['brand']);
+                        array_push($brands, $row['brand']);
                         //9 - Opis produktu
                         array_push($product, $row['productDescription']);
                         //10 - Etykieta sklepu
                         array_push($product, $row['store_label']);
                         //11 - Przekątna
                         array_push($product, $row['diagonal']);
+                        array_push($diagonals, $row['diagonal']); 
                         //12 - Procesor
                         array_push($product, $row['processor']);
                         //13 - Układ graficzny
-                        array_push($product, $row['Układ graficzny']);
+                        array_push($product, $row['graphic']);
                         //14 - Kolor
                         array_push($product, $row['color']);
                         //15 - Rozdzielczość
@@ -78,24 +158,30 @@
                         //17 - Opis kategorii
                         $categoryDescription = $row['categoryDescription'];
                         $categoryName = $row[$name];
+                        //18 - Nazwa podkategorii
+                        array_push($product, $row['subcategory_name']); 
+                        //19 - Opis podkategorii
+                        array_push($product, $row['subcategory_description']); 
 
                         array_push($productsArray, $product); 
+                         
 
                     }
                 }
             }
                 
-            
+           $brands = array_unique($brands); 
             $conn->close();
         }
         catch (Exception $e)
         {
             echo $e;
         }
-
+       
         echo<<<OPEN
         <div class="content">
 OPEN;
+echo $query; 
         if (!empty($_GET['category'])){
             echo<<<BREADCRUMB
             <nav aria-label="breadcrumb">
@@ -116,23 +202,25 @@ BREADCRUMB;
             </nav>
 BREADCRUMB;
         }
+
         echo<<<PRODUCER
         <div class="row">
             <div class="col-lg-3">
-            <form action="#" method="post">
+            <form action="../pages/searchProducts.php?&name=value" method="get">
                 <div class="filters pl-3">
                 <h3>Filtruj</h3>
                 <hr />
                 <div class="producer">
                     <h5>Producent</h5>
 PRODUCER;
-        foreach($productsArray as $index=>$p)
+
+        foreach($brands as $index=>$p)
                 {
                     echo<<<PRODUCER
                     <div class="form-check">
                         <input
-                            class="form-check-input" type="radio" name="radioProducer" id="radioProducer$index"/>
-                        <label class="form-check-label" for="radioProducer$index">$p[8]</label>
+                            class="form-check-input" type="radio" name="producer" value= "$p" id="radioProducer$index"/>
+                        <label class="form-check-label" for="radioProducer$index">$p</label>
                     </div>
 PRODUCER;
                 }
@@ -145,7 +233,7 @@ MODEL;
                 {
                     echo<<<MODEL
                     <div class="form-check">
-                        <input class="form-check-input" type="checkbox" value="" id="checkModel$index"/>
+                        <input class="form-check-input" name="model" value= "$p[2]" type="checkbox" id="checkModel$index"/>
                         <label class="form-check-label" for="$index">$p[1] - $p[2]</label>
                     </div>
 MODEL;
@@ -155,12 +243,12 @@ MODEL;
                 <div class="diagonal">
                     <h5>Przekątna</h5>
 DIAGONAL;
-        foreach($productsArray as $index=>$p)
+        foreach($diagonals as $index=>$p)
                 {
                     echo<<<DIAGONAL
                     <div class="form-check">
-                        <input class="form-check-input" type="checkbox" value="" id="checkModel$index"/>
-                        <label class="form-check-label" for="$index">$p[11]"</label>
+                        <input class="form-check-input" name="diagonal" value= "$p" type="checkbox" id="checkModel$index"/>
+                        <label class="form-check-label" for="$index">$p"</label>
                     </div>
 DIAGONAL;
                 }
@@ -169,8 +257,8 @@ DIAGONAL;
                 <div class="price">
                 <h5>Cena</h5>
                 <div class="input-group mb-3">
-                    <input type="number" class="form-control">
-                    <input type="number" class="form-control">
+                    <input type="number" class="form-control" name="priceFrom">
+                    <input type="number" class="form-control" name="priceTo">
                     <div class="input-group-append">
                         <span class="input-group-text">zł</span>
                     </div>
